005cbee5e3a9f5219e8aaf45fbf3bab5639e7771
namanjain2020 <153709313+namanjain2020@users.noreply.github.com>
2024-07-24 19:26:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('You are directed to my profile . Welcome!!');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const intro = document.getElementById('intro');
+    const observer = new IntersectionObserver(function (entries) {
+        if (entries[0].isIntersecting === true) {
+            intro.classList.add('animate');
+        }
+    }, { threshold: [0] });
+
+    observer.observe(intro);
+});
+
+
